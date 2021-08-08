59953b858a1f8d58f906da33d14d87af4004d982
Siriusb24 <Siriusb2324@gmail.com>
2021-08-08 19:17:03 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,15 @@
-const getAllcategories = function (){
 const allCategories = document.getElementById('categories')
 const allChildren = allCategories.children
+
+const getAllcategories = function (){
 return `В списке ${allChildren.length} категории.`
 }
 console.log(getAllcategories())
 
+const list = Array.from(allCategories.children);
+console.log(list)
+for (let elem of list) {
+    const category = elem.firstElementChild.textContent;   
+    const quantityElem = elem.lastElementChild.children.length;
+    console.log(`Категория: ${category} \nКоличество элементов: ${quantityElem}`);
+};
